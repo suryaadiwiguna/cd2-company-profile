2cc6534030b5b8b4acdaa4b794321b3734acae41
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-12-06 23:16:26 +0700

    fading navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,39 @@
 import { Box, Heading, Text, Flex, Fade } from "@chakra-ui/react";
 import Link from "next/link";
-
 export default function Hero(props) {
     const { heading, subtitle, ctaButtonLabel, bgImage } = props
 
     return (
-        <Box pt={'6em'} minH={'340px'} h={'fit-content'} bg={'grey'} px={'2em'} bgPosition={'center'} bgImage={bgImage ? bgImage : null} bgColor={'black'} color={'white'} backgroundSize={'cover'} position={'relative'} alignItems={'center'}>
-            <Fade in={'true'}>
-                <Flex flexDirection={'column'} justifyContent={'space-around'} gap={'60px'} maxWidth={'1024px'} mx={'auto'} py={'3em'} >
-                    <Box zIndex={10} >
-                        <Heading my={'0.5em'}>{heading}</Heading>
-                        {subtitle && <Text>{subtitle}</Text>}
+        <>
+            <Box pt={'6em'} minH={'340px'} h={'fit-content'} bg={'grey'} px={'2em'} bgPosition={'center'} bgImage={bgImage ? bgImage : null} bgColor={'black'} color={'white'} backgroundSize={'cover'} position={'relative'} alignItems={'center'}>
+                <Flex flexDirection={'column'} justifyContent={'space-around'} gap={'60px'} maxWidth={'1024px'} mx={'auto'} py={'3em'}>
+                    <Box zIndex={2} >
+                        <Fade in={true} transition={{ enter: { duration: 0.5 } }}>
+                            <Heading my={'0.5em'}>{heading}</Heading>
+                            {subtitle && <Text>{subtitle}</Text>}
+                        </Fade>
                     </Box>
 
-                    {ctaButtonLabel && (<Box w={'fit-content'} bg={'#FF7757'} h={'fit'} borderRadius={'10px'} zIndex={10}>
-                        <Link href='/products-services'>
-                            <Flex justify={'space-between'} gap={3} mx={5} py={15} alignItems={'center'}>
-                                <Heading fontSize={'medium'}>
-                                    {ctaButtonLabel}
-                                </Heading>
-                                <Text fontSize={'x-large'}>&#8250;</Text>
-                            </Flex>
-                        </Link>
-                    </Box>)}
-                    <Box bg={'black'} position={'absolute'} top={0} left={0} w={'full'} h={'full'} zIndex={1} opacity={'50%'}>
-                    </Box>
-                </Flex>
-            </Fade>
-        </Box>
+                    {ctaButtonLabel && (
+                        <Box zIndex={2}>
+                            <Fade in={true} transition={{ enter: { duration: 0.5 } }}>
+                                <Box w={'fit-content'} bg={'#FF7757'} h={'fit'} borderRadius={'10px'} >
+                                    <Link href='/products-services'>
+                                        <Flex justify={'space-between'} gap={3} mx={5} py={15} alignItems={'center'}>
+                                            <Heading fontSize={'medium'}>
+                                                {ctaButtonLabel}
+                                            </Heading>
+                                            <Text fontSize={'x-large'}>&#8250;</Text>
+                                        </Flex>
+                                    </Link>
+                                </Box>
+                            </Fade>
+                        </Box>
+                    )
+                    }
+                </Flex >
+                <Box bg={'black'} position={'absolute'} top={0} left={0} w={'full'} h={'full'} zIndex={1} opacity={'50%'} />
+            </Box >
+        </>
     )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,22 @@
 import NavbarMenu from "./NavbarMenu"
-import { Box, Flex, Image, Spacer } from "@chakra-ui/react"
+import { Box, Flex, Image, Spacer, Fade } from "@chakra-ui/react"
 import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <Box w={'100%'} h={'4em'} color={'white'} bg={'transparent'} position={'absolute'} top={0} left={0} zIndex={999}>
-            <Flex px={'30px'} h={'full'} maxW={'1024px'} marginX={'auto'} align={'center'}>
-                <Box>
-                    <Link href={"/"}>
-                        <Image src="/logo.svg" boxSize={'130px'}></Image>
-                    </Link>
-                </Box>
-                <Spacer />
-                <NavbarMenu />
-            </Flex>
-        </Box >
+        <Fade in={true} transition={{ enter: { duration: 0.5 } }}>
+            <Box w={'100%'} h={'4em'} color={'white'} bg={'transparent'} position={'absolute'} top={0} left={0} zIndex={999} >
+                <Flex px={'30px'} h={'full'} maxW={'1024px'} marginX={'auto'} align={'center'} >
+                    <Box>
+                        <Link href={"/"}>
+                            <Image src="/logo.svg" boxSize={'130px'}></Image>
+                        </Link>
+                    </Box>
+                    <Spacer />
+                    <NavbarMenu />
+                </Flex>
+            </Box >
+        </Fade>
+
     )
 }
