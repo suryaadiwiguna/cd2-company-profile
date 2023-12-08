dda1a3238aa459489c33ebb934561b898acf294f
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-12-08 19:04:27 +0700

    fix: missing alt in image tag

--- src/components/card/C_CardPerson.jsx
+++ src/components/card/C_CardPerson.jsx
@@ -11,7 +11,7 @@ export default function C_CardPerson({ data }) {
                 {data.map((data, index) => (
                     <Box key={index} borderRadius={'15px'} maxW={'310px'} bgColor={'white'} overflow={'hidden'} boxShadow={'base'}>
                         <Flex p={'1em'} h={'240px'} w={'full'} align={'center'} justify={'center'}>
-                            <Image src={data.profileImg} borderRadius={'50%'} objectFit={'cover'} aspectRatio={1 / 1} maxH={'180px'} />
+                            <Image alt="" src={data.profileImg} borderRadius={'50%'} objectFit={'cover'} aspectRatio={1 / 1} maxH={'180px'} />
                         </Flex>
                         <Flex align={'center'} justify={'center'} direction={'column'} textAlign={'center'} p={'1em'}>
                             <Heading fontSize={'large'} textTransform={'capitalize'} color={'#FF7757'}>{data.name}</Heading>
